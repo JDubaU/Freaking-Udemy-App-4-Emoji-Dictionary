2f0f11217fd5ff4515fea9b280b804e51f3327ac
Freaking Udemy App 2 <you@yourdomain.com>
2017-02-12 14:55:52 -0600

    Added a Table View and second view controller

--- Emoji Dictionary Udemy App 4/ViewController.swift
+++ Emoji Dictionary Udemy App 4/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var emojiArray = ["🙌","👍","💪","👋","👌🏾","👌","🌚","🌝","😆"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojiArray[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
